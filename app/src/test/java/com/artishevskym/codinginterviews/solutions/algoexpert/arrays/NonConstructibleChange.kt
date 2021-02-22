5aa097ff9e1c349de32dc55ea84ba9213cbab469
Martin Artishevsky <artishevsky.m@gmail.com>
2021-02-22 19:49:12 +0000

    Solve Non-Constructible Change question

---
+++ app/src/test/java/com/artishevskym/codinginterviews/solutions/algoexpert/arrays/NonConstructibleChange.kt
@@ -0,0 +1,38 @@
+package com.artishevskym.codinginterviews.solutions.algoexpert.arrays
+
+import org.amshove.kluent.internal.assertEquals
+import org.junit.Test
+
+/**
+ * Find the minimum amount of change (the minimum sum of money) that you cannot create
+ * from given array of positive integers representing the values of coins.
+ * @see <a href="https://www.algoexpert.io/questions/Non-Constructible%20Change">Non-Constructible Change</a>
+ */
+class NonConstructibleChange {
+
+    @Test
+    fun testSortingSolution() {
+        assertEquals(EXPECTED_RESULT, nonConstructibleChange(COINS))
+    }
+
+    // time: O(nlogn) | space: O(1)
+    private fun nonConstructibleChange(coins: MutableList<Int>): Int {
+        coins.sort()
+
+        var currentChange = 0
+        for (coin in coins) {
+            if (coin > currentChange + 1) {
+                return currentChange + 1 // value we cannot create
+            }
+
+            currentChange += coin
+        }
+
+        return currentChange + 1 // it's the next value we cannot create
+    }
+
+    private companion object {
+        private val COINS = mutableListOf(5, 7, 1, 1, 2, 3, 22)
+        private const val EXPECTED_RESULT = 20
+    }
+}
